d6f0490d9f63c4ac97a728013eadf898eec84c51
Mohammadreza Sorouri Khosrosh <mrezas.khosroshahi@CONSOLSYS.INTRANET>
2017-02-16 11:55:40 +0800

    Add project files.

---
+++ GitStart/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello team from git!");
+            Console.Read();
+        }
+    }
+}
